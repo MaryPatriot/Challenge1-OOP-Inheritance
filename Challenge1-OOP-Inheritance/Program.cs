c0288ddae3b2132d536e619d96f6baeeca57ed45
Mary <mary.vatanparast@gmail.com>
2021-02-23 18:44:47 +0100

    Done!

--- Challenge1-OOP-Inheritance/Program.cs
+++ Challenge1-OOP-Inheritance/Program.cs
@@ -10,16 +10,20 @@ namespace Challenge1_OOP_Inheritance
     {
         static void Main(string[] args)
         {
-            Cat cat1 = new Cat(12);
-            Cat cat2 = new Cat(4);
-            Cat cat3 = new Cat(8);
-            int x = cat1.Age;
-            int y = cat2.Age;
-            int z = cat3.Age;
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Cat.AgeCom(x, y, z);
+            Cat cat1 = new Cat(16);
+            Cat cat2 = new Cat(34);
+            Cat cat3 = new Cat(9);
+            //int x = cat1.Age;
+            //int y = cat2.Age;
+            //int z = cat3.Age;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+            Console.WriteLine(cat1.Age);
+            Console.WriteLine(cat2.Age);
+            Console.WriteLine(cat3.Age);
+            Cat.AgeCom(cat1.Age, cat2.Age, cat3.Age);
 
 
         }
