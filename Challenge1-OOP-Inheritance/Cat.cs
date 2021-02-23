5c5ecbcb245706573401805b625954fb3fe37e7d
Mary <mary.vatanparast@gmail.com>
2021-02-23 18:31:29 +0100

    Add project files.

---
+++ Challenge1-OOP-Inheritance/Cat.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1_OOP_Inheritance
+{
+    class Cat
+    {
+        private string _species = "mamma1";
+        private int _age;
+
+        public string Species { get => _species; set => _species = value; }
+        public int Age {
+
+            get { return _age; }
+            set {
+                    if (value>0)
+                    {
+                        _age = value;
+                    }
+            }
+              
+        }
+
+       public Cat(int _age)
+        {
+            Age = _age;
+        }
+
+        public static void AgeCom(int x, int y, int z)
+        {
+            if (x> y && x> z)
+            {
+                Console.WriteLine("Cat1 is oldest cat");
+            }
+            else if (y > x && y > z)
+            {
+                Console.WriteLine("Cat2 is oldest cat");
+            }
+            else
+            {
+                Console.WriteLine("Cat3 is oldest cat");
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Challenge1-OOP-Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1_OOP_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat(12);
+            Cat cat2 = new Cat(4);
+            Cat cat3 = new Cat(8);
+            int x = cat1.Age;
+            int y = cat2.Age;
+            int z = cat3.Age;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Cat.AgeCom(x, y, z);
+
+
+        }
+    }
+}
